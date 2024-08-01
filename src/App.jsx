7cc4032fd0f5e39aa5bf36463ddef3433b442fe6
Lonely <josuepoza.24@gmail.com>
2024-07-31 23:35:17 -0500

    Se actualizó la rutas de la aplicación hasta inventario. Luego actualizar si es necesario.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ import { ProductProvider } from './context/ProductContext';
 import { EntradaProductProvider } from './context/EntradaProdContex';
 import { SalidaProductProvider } from './context/SalidaProdContext';
 import { CategoriaProvider } from './context/CategoriaContext';
+import ProductoForm from './components/ProductoForm';
+import ProveedorForm from './components/ProveedorForm';
+import EntradaProdForm from './components/EntradaProdForm';
+import SalidaProdForm from './components/SalidaProdForm';
+import { ClienteProvider } from './context/ClienteContext';
+import { InventarioProvider } from './context/InventarioContext';
+import Inventario from './Pages/Inventario';
+import InventarioForm from './components/InventarioForm';
 
 
 function App() {
@@ -19,15 +27,25 @@ function App() {
         <EntradaProductProvider>
           <SalidaProductProvider>
             <ProveedorProvider>
-              <BrowserRouter>
-                <Routes>
-                  <Route path='/' element={<Dashboard />}></Route>
-                  <Route path='/producto' element={<Producto />}></Route>
-                  <Route path='/proveedor' element={<Proveedor />}></Route>
-                  <Route path='/entradaProducto' element={<EntradaProducto />}></Route>
-                  <Route path='/salidaProducto' element={<SalidaProducto />}></Route>
-                </Routes>
-              </BrowserRouter>
+              <ClienteProvider>
+                <InventarioProvider>
+                  <BrowserRouter>
+                    <Routes>
+                      <Route path='/' element={<Dashboard />}></Route>
+                      <Route path='/producto' element={<Producto />}></Route>
+                      <Route path='/producto/:id' element={<ProductoForm />}></Route>
+                      <Route path='/proveedor' element={<Proveedor />}></Route>
+                      <Route path='/proveedor/:id' element={<ProveedorForm />}></Route>
+                      <Route path='/entradaProducto' element={<EntradaProducto />}></Route>
+                      <Route path='/entradaProducto/:id' element={<EntradaProdForm />}></Route>
+                      <Route path='/salidaProducto' element={<SalidaProducto />}></Route>
+                      <Route path='/salidaProducto/:id' element={<SalidaProdForm />}></Route>
+                      <Route path='/inventario/' element={<Inventario />}></Route>
+                      <Route path='/inventario/:id' element={<InventarioForm />}></Route>
+                    </Routes>
+                  </BrowserRouter>
+                </InventarioProvider>
+              </ClienteProvider>
             </ProveedorProvider>
           </SalidaProductProvider>
         </EntradaProductProvider>
