0742c3ecace85d4af0617cd5f051350ed4e648c2
Lonely <josuepoza.24@gmail.com>
2024-08-06 16:38:10 -0500

    Se agregó las rutas de cliente, categoria y snackbar.

--- src/App.jsx
+++ src/App.jsx
@@ -17,40 +17,51 @@ import { ClienteProvider } from './context/ClienteContext';
 import { InventarioProvider } from './context/InventarioContext';
 import Inventario from './Pages/Inventario';
 import InventarioForm from './components/InventarioForm';
+import { SnackbarProvider } from './context/SnackbarContext';
+import ClienteForm from './components/ClienteForm';
+import CategoriaForm from './components/CategoriaForm';
+import Cliente from './Pages/Cliente';
+import Categoria from './Pages/Categoria';
 
 
 function App() {
 
   return (
-    <ProductProvider>
-      <CategoriaProvider>
-        <EntradaProductProvider>
-          <SalidaProductProvider>
-            <ProveedorProvider>
-              <ClienteProvider>
-                <InventarioProvider>
-                  <BrowserRouter>
-                    <Routes>
-                      <Route path='/' element={<Dashboard />}></Route>
-                      <Route path='/producto' element={<Producto />}></Route>
-                      <Route path='/producto/:id' element={<ProductoForm />}></Route>
-                      <Route path='/proveedor' element={<Proveedor />}></Route>
-                      <Route path='/proveedor/:id' element={<ProveedorForm />}></Route>
-                      <Route path='/entradaProducto' element={<EntradaProducto />}></Route>
-                      <Route path='/entradaProducto/:id' element={<EntradaProdForm />}></Route>
-                      <Route path='/salidaProducto' element={<SalidaProducto />}></Route>
-                      <Route path='/salidaProducto/:id' element={<SalidaProdForm />}></Route>
-                      <Route path='/inventario/' element={<Inventario />}></Route>
-                      <Route path='/inventario/:id' element={<InventarioForm />}></Route>
-                    </Routes>
-                  </BrowserRouter>
-                </InventarioProvider>
-              </ClienteProvider>
-            </ProveedorProvider>
-          </SalidaProductProvider>
-        </EntradaProductProvider>
-      </CategoriaProvider>
-    </ProductProvider>
+    <SnackbarProvider>
+      <ProductProvider>
+        <CategoriaProvider>
+          <EntradaProductProvider>
+            <SalidaProductProvider>
+              <ProveedorProvider>
+                <ClienteProvider>
+                  <InventarioProvider>
+                    <BrowserRouter>
+                      <Routes>
+                        <Route path='/' element={<Dashboard />}></Route>
+                        <Route path='/producto' element={<Producto />}></Route>
+                        <Route path='/producto/:id' element={<ProductoForm />}></Route>
+                        <Route path='/proveedor' element={<Proveedor />}></Route>
+                        <Route path='/proveedor/:id' element={<ProveedorForm />}></Route>
+                        <Route path='/entradaProducto' element={<EntradaProducto />}></Route>
+                        <Route path='/entradaProducto/:id' element={<EntradaProdForm />}></Route>
+                        <Route path='/salidaProducto' element={<SalidaProducto />}></Route>
+                        <Route path='/salidaProducto/:id' element={<SalidaProdForm />}></Route>
+                        <Route path='/inventario/' element={<Inventario />}></Route>
+                        <Route path='/inventario/:id' element={<InventarioForm />}></Route>
+                        <Route path='/cliente/' element={<Cliente />}></Route>
+                        <Route path='/cliente/:id' element={<ClienteForm />}></Route>
+                        <Route path='/categoria/' element={<Categoria />}></Route>
+                        <Route path='/categoria/:id' element={<CategoriaForm />}></Route>
+                      </Routes>
+                    </BrowserRouter>
+                  </InventarioProvider>
+                </ClienteProvider>
+              </ProveedorProvider>
+            </SalidaProductProvider>
+          </EntradaProductProvider>
+        </CategoriaProvider>
+      </ProductProvider>
+    </SnackbarProvider>
   )
 }
 
